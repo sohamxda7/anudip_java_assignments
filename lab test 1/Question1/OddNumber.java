fd1a1e58c7c77f019b82c1c47d09973971b546b5
Soham Sen <sensoham135@gmail.com>
2023-02-02 13:58:02 +0530

    assignment : upload question 1 of lab test 1
    the question was as follows:
    WAP to print all odd numbers between 1 to 20.(write logic in separate method and call this method in main())

---
+++ lab test 1/Question1/OddNumber.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.odd;
+
+/**
+ *
+ * @author rick
+ */
+public class OddNumber {
+    public static void printOddNumbers() {
+    for (int i = 1; i <= 20; i++) {
+        if (i % 2 != 0) {
+            System.out.println(i);
+        }
+    }
+}
+    
+}
